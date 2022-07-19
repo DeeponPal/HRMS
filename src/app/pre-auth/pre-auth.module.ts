c5726841676c72cfc3c274bdfb934992112ae73d
Arpan <arpandas1402@gmail.com>
2022-07-20 00:54:57 +0530

    Login form template created

--- src/app/pre-auth/pre-auth.module.ts
+++ src/app/pre-auth/pre-auth.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PreAuthRoutingModule } from './pre-auth-routing.module';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     PreAuthRoutingModule
